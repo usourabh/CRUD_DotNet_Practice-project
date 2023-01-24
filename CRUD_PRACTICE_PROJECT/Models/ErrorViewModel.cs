6ac6107c93acedcf6923358c9af1ec7855f63de5
Sourabh Upadhyay <Usourabh99@gmail.com>
2023-01-24 17:17:45 +0530

    Initial commit

---
+++ CRUD_PRACTICE_PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_PRACTICE_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
